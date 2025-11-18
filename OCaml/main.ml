2eeaa034c472a0dd6ace2376116a6019e8310d04
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2025-11-18 11:43:04 +0000

    add comments and a type alias for buckets

--- OCaml/main.ml
+++ OCaml/main.ml
@@ -1,6 +1,7 @@
 let max_output = 200
 
-let words =
+(* Turn input into a list of words. Keep punctuation attached to words. *)
+let input =
   Str.split (Str.regexp "[ \t\r\n]+") (In_channel.input_all In_channel.stdin)
 
 module Prefix = struct
@@ -11,22 +12,27 @@ end
 
 module PrefixMap = Map.Make (Prefix)
 
+(* Collisions will be stored in Dynarrays *)
+type suffix_bucket = string option Dynarray.t
+
 let build_state_map =
-  let rec build_state_map' (map : string option Dynarray.t PrefixMap.t)
-      (words : string list) =
-    match words with
+  let rec build_state_map' (map : suffix_bucket PrefixMap.t)
+      (input : string list) =
+    match input with
     | w1 :: w2 :: t -> begin
         let prefix = (w1, w2) in
         let w3 = if t = [] then None else Some (List.hd t) in
-        let str_arr =
+        let bucket =
           match PrefixMap.find_opt prefix map with
           | None -> Dynarray.make 1 w3
+          (* Intentionally allow duplicates in a bucket. This makes more common
+          suffixes naturally more common in the generated output. *)
           | Some arr ->
               Dynarray.add_last arr w3;
               arr
         in
-        let map' = PrefixMap.add prefix str_arr map in
-        build_state_map' map' (List.tl words)
+        let map' = PrefixMap.add prefix bucket map in
+        build_state_map' map' (List.tl input)
       end
     | _ -> map
   in
@@ -39,22 +45,23 @@ let make_prefix = function
   | _ -> raise (Invalid_argument "Input shorter than prefix length")
 
 let generate n =
-  let start_prefix = make_prefix words in
+  (* Start off generation with the same start as the input text *)
+  let start_prefix = make_prefix input in
   let () = Printf.printf "%s %s " (fst start_prefix) (snd start_prefix) in
   let rec generate' n state_map prefix =
     if n = 0 then print_newline ()
     else
       match PrefixMap.find_opt prefix state_map with
       | None -> print_newline ()
-      | Some dynArr -> begin
-          let idx = Random.int (Dynarray.length dynArr) in
-          match Dynarray.get dynArr idx with
+      | Some bucket -> begin
+          let idx = Random.int (Dynarray.length bucket) in
+          match Dynarray.get bucket idx with
           | None -> print_newline ()
           | Some str ->
               Printf.printf "%s " str;
               generate' (n - 1) state_map (snd prefix, str)
         end
   in
-  generate' n (build_state_map words) start_prefix
+  generate' n (build_state_map input) start_prefix
 
 let () = generate max_output
